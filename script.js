4d9419ede6cee3531f3bf436aaac96ed9224fe4f
Tanzim Wadi <tanzimjafirwadi@gmail.com>
2025-06-04 04:56:54 +0600

    Update script.js

--- script.js
+++ script.js
@@ -19,3 +19,26 @@ function BeerOnTheWall(numberOfBottle) {
 }
 
 BeerOnTheWall(99);
+
+//while loop end here
+
+// Now the same code is written using the "for" loop
+
+function BeerOnTheWall(NewNumber) {
+    for (i = NewNumber; i >= 1; i--) {
+        var bottleWord = i === 1 ? "bottle": "bottles"; 
+        console.log(i + " " + bottleWord + " of beer on the wall, " + i + " " + bottleWord + " of beer,");
+        console.log("takes one down, and passes it around,");
+
+        //recalculate the number of bottles
+        bottleWord = (i - 1) === 1 ? "bottle" : "bottles";
+        if ((i - 1) >= 1) {
+            console.log((i - 1) + " " + bottleWord + " of beer on the wall.");
+        } else {
+            console.log("No more bottles of beer on the wall, no more bottles of beer,");
+            console.log("Go to store and boy some more, 99 bottles of beer on the wall.");
+        }
+    }
+    console.log(i);
+}
+BeerOnTheWall(99);
